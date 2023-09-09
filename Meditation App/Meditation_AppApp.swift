5d9198a6d696aba9b86db33378fd4c3c27c78998
Clement Ojiguo <clementojigs@hotmail.com>
2023-09-09 12:08:00 +0100

    Initial Commit

---
+++ Meditation App/Meditation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meditation_AppApp.swift
+//  Meditation App
+//
+//  Created by Clem OJ on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Meditation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
